7846f84e16df5efb8fd7128565f7c899cb5e58f3
stimmy <glab@stimmy>
2016-01-20 17:14:58 -0800

    use Anwar's online moments calculator to get mean and std using minimal memory

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import cPickle
 from glob import glob
+from itertools import izip
 import time
 import yaml
 import argparse
@@ -157,20 +158,6 @@ class ApplyMask(object):
 	def run(self, volume):
 		assert np.allclose(volume.affine, self.mask_affine)
 		return volume.get_data().T[self.mask.T]
-		
-
-class Average(object):
-	def __init__(self):
-		self.average_image = None
-		self.n_images = 0
-
-	def run(self, input_image):
-
-		if self.n_images==0:
-			self.average_image = input_image
-			return self.average_image
-			
-		return self.average_image
 
 class WMDetrend(object):
 	'''
@@ -230,19 +217,117 @@ class WMDetrend(object):
 		gm_trend = self.model.predict(wm_activity_pcs)
 		return gm_activity - gm_trend
 
-class RunningMeanStd(object):
-	def __init__(self, n=20):
-		self.n = n
-		self.mean = None
-	def run(self, inp):
-		if self.mean is None:
-			self.samples = np.empty((self.n, inp.size))*np.nan
-		else:
-			self.samples[:-1,:] = self.samples[1:,:]
-		self.samples[-1,:] = inp
-		self.mean = np.nanmean(self.samples, 0)
-		self.std = np.nanstd(self.samples, 0)
-		return self.mean, self.std
+def compute_raw2var(raw1,raw2,*args):
+  '''Use the raw moments to compute the 2nd central moment
+  VAR(X) = E[X^2] - E[X]^2
+  '''
+  return raw2 - raw1**2
+def compute_raw2skew(raw1,raw2,raw3,*args):
+  '''Use the raw moments to compute the 3rd standardized moment
+  Skew(X) = (E[X^3] - 3*E[X]*E[X^2] + 2*E[X]^3)/VAR(X)^(3/2)
+  '''
+  # get central moments
+  cm2 = raw2 - raw1**2
+  cm3 = raw3 - 3*raw1*raw2 + 2*raw1**3
+  # get standardized 3rd moment
+  sm3 = cm3/cm2**1.5
+  return sm3
+
+
+def compute_raw2kurt(raw1,raw2,raw3,raw4, *args):
+  '''Use the raw moments to compute the 4th standardized moment
+  Kurtosis(X) = (E[X^4] - 4*E[X]*E[X^3] + 6*E[X]^2*E[X^2] - 3*E[X]^4)/VAR(X)^2 - 3.0
+  '''
+  # get central moments
+  cm2 = raw2 - raw1**2
+  cm4 = raw4 - 4*raw1*raw3 + 6*(raw1**2)*raw2 - 3*raw1**4
+  # get standardized 4th moment
+  sm4 = cm4/cm2**2 - 3
+  return sm4
+def convert_parallel2moments(node_raw_moments, nsamples):
+  '''Combine the online parallel computations of
+  `online_moments` objects to compute moments.
+
+  Parameters
+  -----------
+  node_raw_moments : list
+    Each element in the list is a node output.
+    Each node output is a `len(4)` object where the
+    Nth element is Nth raw moment sum: `np.sum(X**n)`.
+    Node moments are aggregated across nodes to compute:
+    mean, variance, skewness, and kurtosis.
+  nsamples : scalar
+    The total number of samples across nodes
+
+
+  Returns
+  -------
+  mean : array-like
+    The mean of the full distribution
+  variance: array-like
+    The variance of the full distribution
+  skewness: array-like
+    The skewness of the full distribution
+  kurtosis: array-like
+    The kurtosis of the full distribution
+  '''
+  mean_moments = []
+  for raw_moment in izip(*node_raw_moments):
+    moment = np.sum(raw_moment, 0)/nsamples
+    mean_moments.append(moment)
+
+  emean = mean_moments[0]
+  evar = compute_raw2var(*mean_moments)
+  eskew = compute_raw2skew(*mean_moments)
+  ekurt = compute_raw2kurt(*mean_moments)
+  return emean, evar, eskew, ekurt
+
+class OnlineMoments(object):
+  '''Compute 1-Nth raw moments online
+
+  For the Ith moment: E[X^i] = (1/n)*\Sum(X^i)
+  This function only stores \Sum(X^i) and keeps
+  track of the number of observations.
+  '''
+  def __init__(self, order=4):
+    self.n = 0.0
+    self.order = order
+    self.all_raw_moments = [0.0]*self.order
+    for odx in xrange(self.order):
+      self.__setattr__('rawmnt%i'%(odx+1), self.all_raw_moments[odx])
+
+
+  def __repr__(self):
+    return '%s.online_moments <%i samples>' % (__name__, int(self.n))
+
+  def update(self, x):
+    '''Update the raw moments
+
+    Parameters
+    ----------
+    x : np.ndarray, or scalar-like
+      The new observation. This can be any dimension.
+    '''
+    self.n += 1
+
+    for odx in xrange(self.order):
+      name = 'rawmnt%i'%(odx+1)
+      self.all_raw_moments[odx] = self.all_raw_moments[odx] + x**(odx+1)
+      self.__setattr__(name, self.all_raw_moments[odx])
+
+  def get_statistics(self):
+    '''Return the 1,2,3,4-moment estimates'''
+    return convert_parallel2moments([self.get_raw_moments()[:4]], self.n) # mean,var,skew,kurt
+
+  def get_raw_moments(self):
+    return self.all_raw_moments
+
+  def get_norm_raw_moments(self):
+    return map(lambda x: x/float(self.n), self.all_raw_moments)
+
+  def run(self, inp):
+  	self.update(inp)
+  	return self.get_statistics()[:2]
 		
 class VoxelZScore(object):
 	def __init__(self):
