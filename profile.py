f607fec0f96644b8185a5b3b86e8e9b191cf2fe0
Robert Gibboni <galileo@gmail.com>
2016-02-17 18:12:34 -0800

    cleaned up profiling code

--- profile.py
+++ profile.py
@@ -15,23 +15,24 @@ def parse_log(log_path):
     return log
 
 if __name__=='__main__':
+    
     log = parse_log(log_path)
     log = pd.DataFrame(log, columns=['time', 'name', 'level', 'message'])
     log['time'] = pd.to_datetime(log.time, format='%Y-%m-%d %H:%M:%S,%f')
-    ix = log.message.str.startswith('received').nonzero()[0][:2]
-    first_image = log.ix[ix[0]:ix[1]]
-    steps = first_image.ix[first_image.message.str.startswith('running')]
-    step_names = steps.message.str.replace('running ', '')
+    
     starts = log.ix[log.message.str.startswith('running')]
     finishes = log.ix[log.message.str.startswith('finished')]
     starts.index = np.arange(len(starts))
     finishes.index = np.arange(len(finishes))
+    
     dtime = starts.join(finishes, lsuffix='_start', rsuffix='_finish')
     dtime['d_seconds'] = (dtime.time_finish - dtime.time_start).apply(lambda x: x / np.timedelta64(1,'s'))
-    dtime['step_type'] = dtime.message_start.str.replace('running ', '')
-    dtime_gp = dtime.groupby('step_type').d_seconds
+    dtime['step_name'] = dtime.message_start.str.replace('running ', '')
+    dtime_gp = dtime.groupby('step_name').d_seconds
+    
     summary = pd.DataFrame([dtime_gp.mean(), dtime_gp.min(), dtime_gp.max()]).T
     summary.columns = ['mean', 'min', 'max']
+    
     print 'Summary per preprocessing step:\n'
     print summary
     print '\nAverage total time:'
