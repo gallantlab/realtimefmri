2b36a025406024fbde59d9ab28da6206667511e6
robert <galileo@gmail.com>
2017-01-03 14:22:47 -0800

    added README

--- realtimefmri/core/collection.py
+++ realtimefmri/core/collection.py
@@ -38,123 +38,123 @@ class DataCollector(object):
 		s = ctx.socket(zmq.REP)
 		s.bind('tcp://*:%d'%port)
 		logger.info('waiting for image subscriber to initialize sync')
-		s.recv()
-		s.send('READY!')
-		logger.info('synchronized with image subscriber')
+        s.recv()
+        s.send('READY!')
+        logger.info('synchronized with image subscriber')
 
-	def _sync_with_image_acq(self):
-		logger.info('waiting for image')
-		self.image_acq.recv()
-		logger.info('acquired image')
-		if self._t0 is None:
-			self._t0 = time.time()
-			logger.info('synchronized with first image at time %.2f'%self._t0)
-		return time.time()-self._t0
+    def _sync_with_image_acq(self):
+        logger.info('waiting for image')
+        self.image_acq.recv()
+        logger.info('acquired image')
+        if self._t0 is None:
+            self._t0 = time.time()
+            logger.info('synchronized with first image at time %.2f'%self._t0)
+        return time.time()-self._t0
 
-	def _simulate(self, interval, subject):
-		ex_dir = get_example_data_directory(subject)
-		logger.info('simulating from %s' % ex_dir)
-		image_fpaths = glob(os.path.join(ex_dir, '*.PixelData'))
-		image_fpaths.sort()
-		image_fpaths = cycle(image_fpaths)
+    def _simulate(self, interval, subject):
+        ex_dir = get_example_data_directory(subject)
+        logger.info('simulating from %s' % ex_dir)
+        image_fpaths = glob(os.path.join(ex_dir, '*.PixelData'))
+        image_fpaths.sort()
+        image_fpaths = cycle(image_fpaths)
 
-		if interval=='sync':
-			ctx = zmq.Context.instance()
-			s = ctx.socket(zmq.PULL)
-			s.connect('tcp://localhost:5554')
+        if interval=='sync':
+            ctx = zmq.Context.instance()
+            s = ctx.socket(zmq.PULL)
+            s.connect('tcp://localhost:5554')
 
-		for image_fpath in image_fpaths:
-			if interval=='return':
-				raw_input('>> Press return for next image')
-			elif interval=='sync':
-				t = self._sync_with_image_acq()
-				time.sleep(0.2) # simulate image scan and reconstruction time
-			else:
-				time.sleep(interval)
+        for image_fpath in image_fpaths:
+            if interval=='return':
+                raw_input('>> Press return for next image')
+            elif interval=='sync':
+                t = self._sync_with_image_acq()
+                time.sleep(0.2) # simulate image scan and reconstruction time
+            else:
+                time.sleep(interval)
 
-			with open(image_fpath, 'r') as f:
-				raw_image_binary = f.read()
-			logger.info(os.path.basename(image_fpath))
-			self.image_pub.send_multipart([b'image', struct.pack('d', t), raw_image_binary])
+            with open(image_fpath, 'r') as f:
+                raw_image_binary = f.read()
+            logger.info(os.path.basename(image_fpath))
+            self.image_pub.send_multipart([b'image', struct.pack('d', t), raw_image_binary])
 
-	def _run(self):
-		self.active = True
-		self.monitor = MonitorDirectory(self.directory, image_extension='.PixelData')
-		while self.active:
+    def _run(self):
+        self.active = True
+        self.monitor = MonitorDirectory(self.directory, image_extension='.PixelData')
+        while self.active:
                         new_image_paths = None
-			t = self._sync_with_image_acq()
-			while not new_image_paths:
-				new_image_paths = self.monitor.get_new_image_paths()
-				if len(new_image_paths)>0:
-					with open(os.path.join(self.directory, list(new_image_paths)[0]), 'r') as f:
-						raw_image_binary = f.read()
-					msg = 'image '+raw_image_binary
-					self.image_pub.send_multipart([b'image', t, raw_image_binary])
-					self.monitor.update(new_image_paths)
-				time.sleep(0.1)
-	
-	def run(self):
-		# watch the parent_directory for the first new directory, then use that as the directory to monitor
-		if self.parent_directory:
-			m = MonitorDirectory(self.directory, image_extension='/')
-			while True:
-				new_image_paths = m.get_new_image_paths()
-				if len(new_image_paths)>0:
-					self.directory = os.path.join(self.directory, new_image_paths.pop())
-					logger.info('detected new folder %s, monitoring' % self.directory)
-					break
-				time.sleep(0.2)
-		self._run()
+            t = self._sync_with_image_acq()
+            while not new_image_paths:
+                new_image_paths = self.monitor.get_new_image_paths()
+                if len(new_image_paths)>0:
+                    with open(os.path.join(self.directory, list(new_image_paths)[0]), 'r') as f:
+                        raw_image_binary = f.read()
+                    msg = 'image '+raw_image_binary
+                    self.image_pub.send_multipart([b'image', t, raw_image_binary])
+                    self.monitor.update(new_image_paths)
+                time.sleep(0.1)
+    
+    def run(self):
+        # watch the parent_directory for the first new directory, then use that as the directory to monitor
+        if self.parent_directory:
+            m = MonitorDirectory(self.directory, image_extension='/')
+            while True:
+                new_image_paths = m.get_new_image_paths()
+                if len(new_image_paths)>0:
+                    self.directory = os.path.join(self.directory, new_image_paths.pop())
+                    logger.info('detected new folder %s, monitoring' % self.directory)
+                    break
+                time.sleep(0.2)
+        self._run()
 
 class MonitorDirectory(object):
-	'''
-	monitor the file contents of a directory
-	Example usage:
-		m = MonitorDirectory(dir_path)
-		# add a file to that directory
-		new_image_paths = m.get_new_image_paths()
-		# use the new images
-		# update image paths list to contain newly acquired images
-		m.update(new_image_paths)
-		# no images added
-		new_image_paths = m.get_new_image_paths()
-		len(new_image_paths)==0 # True
-	'''
-	def __init__(self, directory, image_extension='.PixelData'):
-		logger.debug('monitoring %s for %s' % (directory, image_extension))
+    '''
+    monitor the file contents of a directory
+    Example usage:
+        m = MonitorDirectory(dir_path)
+        # add a file to that directory
+        new_image_paths = m.get_new_image_paths()
+        # use the new images
+        # update image paths list to contain newly acquired images
+        m.update(new_image_paths)
+        # no images added
+        new_image_paths = m.get_new_image_paths()
+        len(new_image_paths)==0 # True
+    '''
+    def __init__(self, directory, image_extension='.PixelData'):
+        logger.debug('monitoring %s for %s' % (directory, image_extension))
 
-		if image_extension=='/':
-			self._is_valid = self._is_valid_directories
-		else:
-			self._is_valid = self._is_valid_files
+        if image_extension=='/':
+            self._is_valid = self._is_valid_directories
+        else:
+            self._is_valid = self._is_valid_files
 
-		self.directory = directory
-		self.image_extension = image_extension
-		self.image_paths = self.get_directory_contents()
+        self.directory = directory
+        self.image_extension = image_extension
+        self.image_paths = self.get_directory_contents()
 
-	def _is_valid_directories(self, val):
-		return os.path.isdir(os.path.join(self.directory, val))
-	def _is_valid_files(self, val):
-		return val.endswith(self.image_extension)
+    def _is_valid_directories(self, val):
+        return os.path.isdir(os.path.join(self.directory, val))
+    def _is_valid_files(self, val):
+        return val.endswith(self.image_extension)
 
-	def get_directory_contents(self):
-		'''
-		returns entire contents of directory with image_extension
-		'''
-		return set([i for i in os.listdir(self.directory) if self._is_valid(i)])
+    def get_directory_contents(self):
+        '''
+        returns entire contents of directory with image_extension
+        '''
+        return set([i for i in os.listdir(self.directory) if self._is_valid(i)])
 
-	def get_new_image_paths(self):
-		'''
-		gets entire contents of directory and returns paths that were not present since last update
-		'''
-		directory_contents = self.get_directory_contents()
-		if len(directory_contents)>len(self.image_paths):
-			new_image_paths = set(directory_contents) - self.image_paths
-		else: new_image_paths = set()
+    def get_new_image_paths(self):
+        '''
+        gets entire contents of directory and returns paths that were not present since last update
+        '''
+        directory_contents = self.get_directory_contents()
+        if len(directory_contents)>len(self.image_paths):
+            new_image_paths = set(directory_contents) - self.image_paths
+        else: new_image_paths = set()
 
-		return new_image_paths
+        return new_image_paths
 
-	def update(self, new_image_paths):
-		if len(new_image_paths)>0:
-			logger.debug(new_image_paths)
-			self.image_paths = self.image_paths.union(new_image_paths)
+    def update(self, new_image_paths):
+        if len(new_image_paths)>0:
+            logger.debug(new_image_paths)
+            self.image_paths = self.image_paths.union(new_image_paths)
--- realtimefmri/preprocess.py
+++ realtimefmri/preprocess.py
@@ -7,12 +7,12 @@ logger = get_logger('preprocess', dest=['console', 'file'])
 
 if __name__=='__main__':
 
-	parser = argparse.ArgumentParser(description='Preprocess data')
-	parser.add_argument('config',
-		action='store',
-		help='Name of configuration file')
-	args = parser.parse_args()
-	logger.info('Loading preprocessing pipeline from %s' % args.config)
+    parser = argparse.ArgumentParser(description='Preprocess data')
+    parser.add_argument('config',
+        action='store',
+        help='Name of configuration file')
+    args = parser.parse_args()
+    logger.info('Loading preprocessing pipeline from %s' % args.config)
 
-	preproc = Preprocessor(args.config)
-	preproc.run()
+    preproc = Preprocessor(args.config)
+    preproc.run()
