aad106d9bb1ac9613e7c426452e956d0ae08a243
robert <galileo@gmail.com>
2017-03-30 10:22:13 -0700

    new command line script with subcommands, include actual code

--- realtimefmri/__main__.py
+++ realtimefmri/__main__.py
@@ -1,5 +1,6 @@
 from os import makedirs
 import os.path as op
+from subprocess import Popen
 import argparse
 import asyncio
 import zmq
@@ -9,40 +10,58 @@ from realtimefmri.synchronize import Synchronizer
 from realtimefmri.collect import Collector
 from realtimefmri.scan import Scanner
 from realtimefmri.utils import get_logger
-from realtimefmri.config import RECORDING_DIR
+from realtimefmri.config import RECORDING_DIR, MODULE_DIR
+
 
 def parse_arguments():
     """Run the data collection
     """
     parser = argparse.ArgumentParser(description='Collect data')
-    parser.add_argument('recording_id', action='store', default=None,
-                       help='''An identifier for this recording''')
-    parser.add_argument('-d', '--directory', action='store',
-                        dest='directory', default=None,
-                        help=('''Directory to watch. If simulate is True,
-                                 simulate from this directory'''))
-    parser.add_argument('-p', '--parent_directory', action='store',
-                        dest='parent_directory', default=None,
-                        help=('''Parent directory to watch. If provided,
-                                 monitor the directory for the first new folder,
-                                 then monitor that folder for new files'''))
-    parser.add_argument('-s', '--simulate', action='store_true',
-                        dest='simulate', default=False,
-                        help=('''Simulate a run'''))
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        default=False, dest='verbose',
-                        help=('''Print log messages to console if true'''))
+    subcommand = parser.add_subparsers(title='subcommand', dest='subcommand')
+    console = subcommand.add_parser('console',
+                                    help="""Collect and synchronize""")
+
+    console.set_defaults(command_name='console')
+    console.add_argument('recording_id', action='store', default=None,
+                         help='''An identifier for this recording''')
+    console.add_argument('-d', '--directory', action='store',
+                         dest='directory', default=None,
+                         help=('''Directory to watch. If simulate is True,
+                                  simulate from this directory'''))
+    console.add_argument('-p', '--parent_directory', action='store',
+                         dest='parent_directory', default=None,
+                         help=('''Parent directory to watch. If provided,
+                                  monitor the directory for the first new folder,
+                                  then monitor that folder for new files'''))
+    console.add_argument('-s', '--simulate', action='store_true',
+                         dest='simulate', default=False,
+                         help=('''Simulate a run'''))
+    console.add_argument('-v', '--verbose', action='store_true',
+                         default=False, dest='verbose',
+                         help=('''Print log messages to console if true'''))
+
+
+    preproc = subcommand.add_parser('preprocess',
+                                    help="""Preprocess and stimulate""")
+    preproc.set_defaults(command_name='preprocess')
+    preproc.add_argument('recording_id', action='store',
+                         help='Unique recording identifier for this run')
+    
+    preproc.add_argument('preproc_config', action='store',
+                         help='Name of preprocessing configuration file')
+    
+    preproc.add_argument('stim_config', action='store',
+                         help='Name of stimulus configuration file')
+    
+    preproc.add_argument('-v', '--verbose', action='store_true',
+                         dest='verbose', default=False)
 
     args = parser.parse_args()
     
-    return {'recording_id': args.recording_id,
-            'directory': args.directory,
-            'parent_directory': args.parent_directory,
-            'simulate': args.simulate,
-            'verbose': args.verbose}
+    return args
 
-def main(recording_id=None, directory=None, parent_directory=None,
-         simulate=False, verbose=False):
+def console(recording_id, directory=None, parent_directory=None, simulate=False,
+            verbose=False):
 
     log_path = op.join(RECORDING_DIR, recording_id, 'recording.log')
     if not op.exists(op.dirname(log_path)):
@@ -78,5 +97,44 @@ def main(recording_id=None, directory=None, parent_directory=None,
         loop.close()
 
 
+def preprocess(recording_id, preproc_config=None, stim_config=None,
+         verbose=None):
+    """Run realtime"""
+
+    processes = []
+
+    # Preprocessing
+    opts = [preproc_config, recording_id]
+    if verbose:
+        opts.append('-v')
+    proc = Popen(['python', op.join(MODULE_DIR, 'preprocess.py')] +
+                 opts)
+    processes.append(proc)
+
+    # Stimulation
+    opts = [stim_config, recording_id]
+    if verbose:
+        opts.append('-v')
+    proc = Popen(['python', op.join(MODULE_DIR, 'stimulate.py')] +
+                 opts)
+    processes.append(proc)
+
+    try:
+        input('running...')
+    except KeyboardInterrupt:
+        print('shutting down realtimefmri')
+
+
+def main(args):
+
+    if args.subcommand == 'console':
+        console(args.recording_id, directory=args.directory,
+                parent_directory=args.parent_directory, simulate=args.simulate,
+                verbose=args.verbose)
+    elif args.subcommand == 'preprocess':
+        preprocess(args.recording_id, preproc_config=args.preproc_config,
+                   stim_config=args.stim_config, verbose=args.verbose)
+
+
 if __name__ == '__main__':
-    main(**parse_arguments())
+    main(parse_arguments())
