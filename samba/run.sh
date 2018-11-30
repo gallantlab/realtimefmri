271038e85899a244ca9d7c408a4a941885ae9f40
Robert Gibboni <galileo@gmail.com>
2018-11-30 22:24:36 +0000

    BUG: make shared directory owned by rtfmri

--- samba/run.sh
+++ samba/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 SAMBA_USER=rtfmri
 SAMBA_PASSWORD=password
-adduser --disabled-password --gecos "" rtfmri
 echo -ne "$SAMBA_PASSWORD\n$SAMBA_PASSWORD\n" | smbpasswd -a -s $SAMBA_USER
 service smbd start
 tail -F /var/log/samba/log.smbd
