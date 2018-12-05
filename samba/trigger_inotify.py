f7a4c1c930c34e94f301fc66cb188f66d3b3fa94
Robert Gibboni <galileo@gmail.com>
2018-12-05 12:18:37 -0800

    MAINT: trigger inotify events on the samba drive

--- samba/trigger_inotify.py
+++ samba/trigger_inotify.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python
 import os
 import os.path as op
+import pwd
 import re
 import subprocess
 import time
 import logging
 
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('trigger_inotify')
 
+user = pwd.getpwuid(os.getuid()).pw_name
+logger.info("Running as user {}".format(user))
 
-def collect_volumes_poll(directory=None, parent_directory=None, extension=None):
-    """Continuously read the monitor directory and add publish new files as they 
-    are detected.
+
+def trigger_inotify(directory=None, parent_directory=None, extension='*'):
+    """Continuously monitor a samba mounted directory for new files and trigger inotify actions.
+
+    File creation on samba network shares do not trigger the same inotify events as regular files. 
+    This function monitors a samba shared directory for new files. When a new file is detected, 
+    open and close it to result in inotify events being triggered.
+
+    Parameters
+    ----------
+    directory : str
+        Directory to monitor for new files
+    parent_directory : str
+        If provided, monitor the first new directory created in this parent directory
+    extension : str
+        Only detect new files with this extension
     """
     if (directory is None) and (parent_directory is None):
         raise ValueError("Must provide either directory or parent_directory.")
 
     if parent_directory:
-        monitor = DirectoryMonitorPoll(parent_directory, extension='/')
+        monitor = MonitorSambaDirectory(parent_directory, extension='/')
         logger.info('Detecting next subfolder in %s' % parent_directory)
         directory = op.join(parent_directory, next(monitor.yield_new_paths()))
         logger.info('Detected %s' % directory)
 
-    monitor = DirectoryMonitorPoll(directory, extension=extension)
+    monitor = MonitorSambaDirectory(directory, extension=extension)
 
     for new_path in monitor.yield_new_paths():
         logger.info('Volume %s' % new_path)
@@ -31,27 +47,34 @@ def collect_volumes_poll(directory=None, parent_directory=None, extension=None):
             pass
 
 
-class DirectoryMonitorPoll(object):
+class MonitorSambaDirectory(object):
     """
-    Monitor the file contents of a directory
+    Monitor the file contents of a directory mounted with samba share
 
-    Example
-    -------
-    m = DirectoryMonitorPoll(dir_path)
-    # add a file to that directory
-    new_paths = m.get_new_contents()
-    # use the new images
-    # update image paths list to contain newly acquired images
-    m.update(new_paths)
-    # no images added
-    new_paths = m.get_new_contents()
-    len(new_paths)==0 # True
+    Parameters
+    ----------
+    directory : str
+        The directory to monitor
+    extension : str
+
+    Examples
+    --------
+    Loop that iterates each time a file is detected.
+
+    >>> m = MonitorSambaDirectory('/tmp/test', extension='.dcm')
+    >>> for path in m.yield_new_paths():
+    >>>     print(path)
+    /tmp/test/1.dcm
+    /tmp/test/2.dcm
+    ...
     """
-    def __init__(self, directory, pattern=None, extension='.dcm'):
+    def __init__(self, directory, extension='.dcm'):
         samba_status = SambaStatus(directory)
 
         if extension == '/':
             self._is_valid = self._is_valid_directories
+        elif extension == '*':
+            self._is_valid = lambda p: True
         else:
             self._is_valid = self._is_valid_files
 
@@ -106,12 +129,21 @@ class DirectoryMonitorPoll(object):
 
 
 class SambaStatus(object):
+    """Class to access information output by the `smbstatus` command.
+
+    Parameters
+    ----------
+    directory : str
+        Only return information related to this directory
+    """
     def __init__(self, directory):
         self.directory = directory
         self.open_file_parser = re.compile(b"\d*\s*\d*\s*[A-Z_]*\s*0x\d*\s*[A-Z]*\s*[A-Z]*\s*"
                                            b"%s\s*(?P<path>.*\.dcm).*" % directory)
 
     def get_open_files(self):
+        """Get a list of files that are currently opened by samba clients
+        """
         proc = subprocess.Popen(['smbstatus', '-L'], stdout=subprocess.PIPE)
         proc.stdout.readline()
         proc.stdout.readline()
@@ -129,4 +161,4 @@ class SambaStatus(object):
 
 
 if __name__ == "__main__":
-    collect_volumes_poll(parent_directory='/mnt/scanner', extension='.dcm')
+    trigger_inotify(parent_directory='/mnt/scanner', extension='.dcm')
