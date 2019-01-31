6b661d0d95b5d08dc96c7347abc0677b66808615
Robert Gibboni <galileo@gmail.com>
2019-01-31 17:59:08 +0000

    Basic structure of online model fitting

--- realtimefmri/web_interface/apps/experiment.py
+++ realtimefmri/web_interface/apps/experiment.py
@@ -2,10 +2,14 @@ import numpy as np
 import pickle
 import redis
 import time
+import urllib
 
 from flask import request
 from pathlib import Path
-from realtimefmri import config, utils
+import scipy.stats
+from sklearn import linear_model
+
+from realtimefmri import config, detrend, utils
 from realtimefmri.web_interface.app import app
 
 
@@ -27,7 +31,6 @@ def load_features(feature_name):
     -------
     A list of arrays of feature times and a list of arrays of features
     """
-    messages = []
     features = []
     feature_times = []
     for key in r.scan_iter('log:stimulus:*'):
@@ -50,24 +53,66 @@ def align_features_and_responses(feature_times, features, response_times, respon
     return features, responses
 
 
+@app.server.route('/models')
+def serve_models(methods='GET'):
+    key_prefix = 'db:model:*'
+    model_names = list(r.scan_iter(key_prefix))
+    return b'Models:\n' + b'\n'.join(model_names)
+
+
 @app.server.route('/models/<model_name>')
 def serve_model(model_name, methods=['GET']):
-    key_prefix = 'db:' + model_name
+    key_prefix = 'db:model:' + model_name
     keys = list(r.scan_iter(key_prefix + ':*'))
     return f'{len(keys)} time points for {key_prefix}'
 
 
-@app.server.route('/models/<model_name>/fit', methods=['POST'])
+@app.server.route('/models/<model_name>/fit', methods=['GET', 'POST'])
 def serve_fit_model(model_name):
-    key_prefix = 'db:' + model_name
-    response_times, responses = utils.load_timestamped_array_from_redis(key_prefix)
+    query = urllib.parse.parse_qs(request.query_string.decode('utf-8').lstrip('?'))
+    detrend_type = query.get('detrend_type', None)
+    model_type = query.get('model_type', None)
 
-    # get feature times
-    feature_times, features = load_features('motion_energy')
+    response_times, responses = utils.load_timestamped_array_from_redis('db:responses')
 
-    X, y = align_features_and_responses(feature_times, features, response_times, responses)
+    if detrend_type == ['whitematter']:
+        logger.warning('%s %d', detrend_type, len(response_times))
+        _, wm_responses = utils.load_timestamped_array_from_redis('db:wm_responses')
+        detrender = detrend.WhiteMatterDetrend(n_pcs=10)
+        _ = detrender.fit(responses, wm_responses)
+        responses = detrender.detrend(responses, wm_responses)
 
-    return f'Fitting {model_name}'
+    # # get feature times
+    # feature_times, features = load_features('motion_energy')
+    # X, y = align_features_and_responses(feature_times, features, response_times, responses)
+
+    y = scipy.stats.zscore(responses, axis=0)
+    X = np.random.randn(len(y), 10).astype('float32')
+
+    if model_type == ['ridge']:
+        logger.warning('Fitting model %s', model_name)
+        model = linear_model.LinearRegression()
+        _ = model.fit(X, y)
+        logger.warning('Saving model %s', model_name)
+        r.set(f'db:model:{model_name}', pickle.dumps(model))
+
+    return f'Fitting {model_name} {responses.shape}'
+
+
+@app.server.route('/models/<model_name>/predict', methods=['GET'])
+def serve_predict_model(model_name):
+    query = urllib.parse.parse_qs(request.query_string.decode('utf-8').lstrip('?'))
+    model_type = query.get('model_type', None)
+
+    # # get feature times
+    # feature_times, features = load_features('motion_energy')
+
+    X = np.random.randn(20, 10).astype('float32')
+    model = r.get(f'db:model:{model_name}')
+    model = pickle.loads(model)
+    y_hat = model.predict(X)
+
+    return f'Predicting {model_name} {len(y_hat)}'
 
 
 @app.server.route('/experiment/log/<topic>', methods=['POST'])
@@ -91,8 +136,3 @@ def serve_log(topic):
         r.set(f'log:{topic}:{receive_time}:message', request.json['message'])
 
         return '200'
-
-
-@app.server.route('/models/<model_name>/predict', methods=['POST'])
-def serve_predict_model(model_name):
-    return f'Predicting {model_name}'
