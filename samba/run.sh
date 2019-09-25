09e3d82840e8af468c170827a22c8b26b7756f7e
Matteo Visconti dOC <mvdoc@berkeley.edu>
2019-09-25 16:30:25 -0700

    FIX permissions for writing in /logs

--- samba/run.sh
+++ samba/run.sh
@@ -4,5 +4,5 @@ SAMBA_PASSWORD=password
 echo -ne "$SAMBA_PASSWORD\n$SAMBA_PASSWORD\n" | smbpasswd -a -s $SAMBA_USER
 service smbd start
 
-chown -R rtfmri:rtfmri /mnt/scanner
+chown -R rtfmri:rtfmri /mnt/scanner /logs
 su - rtfmri -c "python /detect_dicoms.py"
