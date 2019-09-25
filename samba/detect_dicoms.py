d4a1481681de3f967a897322447d39e918815f05
Matteo Visconti dOC <mvdoc@berkeley.edu>
2019-09-25 16:37:30 -0700

    CLN remove old samba monitoring class

--- samba/detect_dicoms.py
+++ samba/detect_dicoms.py
@@ -45,7 +45,7 @@ def detect_dicoms(root_directory=None, extension='*'):
     """
     logger.info('Monitoring %s', root_directory)
 
-    monitor = MonitorSambaDirectoryAlt(root_directory, file_glob="*/*.dcm")#extension=extension)
+    monitor = MonitorSambaDirectory(root_directory, file_glob="*/*.dcm")
 
     r = redis.StrictRedis('redis')
 
@@ -63,123 +63,19 @@ class MonitorSambaDirectory(object):
     ----------
     directory : str
         The directory to monitor
-    extension : str
+    file_glob : str
 
     Examples
     --------
     Loop that iterates each time a file is detected.
 
-    >>> m = MonitorSambaDirectory('/tmp/test', extension='.dcm')
+    >>> m = MonitorSambaDirectory('/tmp/test', file_glob='*/*.dcm')
     >>> for path in m.yield_new_paths():
     >>>     print(path)
     /tmp/test/1.dcm
     /tmp/test/2.dcm
     ...
     """
-    def __init__(self, root_directory, extension='.dcm'):
-        samba_status = SambaStatus(root_directory)
-
-        self.root_directory = root_directory
-        self.directories = set(self.get_directories())
-        self.extension = extension
-        self.samba_status = samba_status
-
-        self.last_modtimes = {}
-        self.directory_contents = defaultdict(set)
-
-
-        for directory in self.directories:
-            self.last_modtimes[directory] = 0
-            added_contents, _ = self.get_changed_contents(directory, set(), self.is_valid_file)
-            self.directory_contents[directory] = added_contents
-
-    def get_directories(self):
-        return [p for p in os.listdir(self.root_directory)
-                if op.isdir(op.join(self.root_directory, p))]
-
-    def is_valid_directory(self, val):
-        return op.isdir(op.join(self.root_directory, val))
-
-    def is_valid_file(self, val):
-        if not val.endswith(self.extension):
-            return False
-
-        if val in self.samba_status.get_open_files():
-            return False
-
-        return True
-
-    def get_changed_contents(self, directory, previous_contents, validator):
-        """Gets entire contents of directory and returns paths that were not
-        present since last update
-        """
-        current_contents = set([op.basename(p)
-                                for p in os.listdir(op.join(self.root_directory, directory))
-                                if validator(p)])
-
-        if len(current_contents) > len(previous_contents):
-            added_contents = current_contents - previous_contents
-            removed_contents = set()
-        elif len(current_contents) < len(previous_contents):
-            added_contents = set()
-            removed_contents = previous_contents - current_contents
-        else:
-            added_contents, removed_contents = set(), set()
-
-        return added_contents, removed_contents
-
-    def yield_new_paths(self):
-        while True:
-
-            # any new directories?
-            (added_directories,
-             removed_directories) = self.get_changed_contents('', self.directories,
-                                                              self.is_valid_directory)
-            if len(added_directories) > 0:
-                for directory in added_directories:
-                    logger.info('Adding directory %s', directory)
-                    self.last_modtimes[directory] = 0
-                    self.directories.add(directory)
-
-            if len(removed_directories) > 0:
-                for directory in removed_directories:
-                    logger.info('Removing directory %s', directory)
-                    del self.last_modtimes[directory]
-                    self.directories.remove(directory)
-
-            for directory in self.directories:
-                current_modtime = op.getmtime(op.join(self.root_directory, directory))
-                if current_modtime > self.last_modtimes[directory]:
-                    logger.info('Detected change in %s', directory)
-
-                    (added_paths,
-                     removed_paths) = self.get_changed_contents(directory,
-                                                                self.directory_contents[directory],
-                                                                self.is_valid_file)
-
-                    self.last_modtimes[directory] = current_modtime
-
-                    def get_mod_time(filename):
-                        return op.getmtime(op.join(self.root_directory,
-                                                   directory, filename))
-
-                    if len(added_paths) > 0:
-                        for path in sorted(added_paths, key=get_mod_time):
-                            path = op.basename(path)
-                            logger.info('Adding %s from %s', path, directory)
-                            self.directory_contents[directory].add(path)
-                            yield op.join(self.root_directory, directory, path)
-
-                    if len(removed_paths) > 0:
-                        for path in removed_paths:
-                            path = op.basename(path)
-                            logger.info('Removing %s from %s', path, directory)
-                            self.directory_contents[directory].remove(path)
-
-            time.sleep(0.1)
-
-
-class MonitorSambaDirectoryAlt:
 
     def __init__(self, root_directory, file_glob="*/*.dcm"):
         self.root_directory = root_directory
@@ -210,8 +106,6 @@ class MonitorSambaDirectoryAlt:
                 logger.info(f"Adding {filename} at {time.time()}")
                 yield filename
             time.sleep(.1)
-
-
     
 
 class SambaStatus():
